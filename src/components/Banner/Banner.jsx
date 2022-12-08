e459029c8e55fceabf06036025522b8a33e3b83b
Chimda B <chimdabrignoli@gmail.com>
2022-12-08 23:29:57 +0100

    create banner component wip

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import "./Banner.scss"
+
+
+const Banner = ({imgSrc, altText, title}) => {
+    return (
+        <div className="banner">
+            <img 
+                src= {imgSrc} 
+                alt={altText}
+                className="banner-img" 
+            />
+            <h1 className="banner-title">
+                {title}
+            </h1>
+        </div>
+    )
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Banner from '../../components/Banner/Banner';
+import imgHome from "../../assets/img_home.png";
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Banner 
+                imgSrc={imgHome}
+                altText={"illustration au bord de mer"}
+                title={"Chez vous, partout et ailleurs"}
+            />
         </div>
     )
 }
