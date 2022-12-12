4d89ff17fda60471083e19936cda6d8401d78834
Chimda B <chimdabrignoli@gmail.com>
2022-12-12 21:33:35 +0100

    page not found works

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./notfound.scss";
 
 const NotFound = () => {
     return (
         <div className="notfound">
             <p className="notfound-title">404</p>
-            <p className="notfound-text">Oups! La page que vous demandez n'existe pas</p>
+            <p className="notfound-text">Oups ! La page que vous demandez n'existe pas</p>
             <p className="notfound-link">
                 <Link
                 className="notfound-link-text"
