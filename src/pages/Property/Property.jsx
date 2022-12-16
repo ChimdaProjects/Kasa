8679343989e750e7027728d6dd4618d2ad9dd450
Chimda B <chimdabrignoli@gmail.com>
2022-12-16 20:38:32 +0100

    page property

--- src/pages/Property/Property.jsx
+++ src/pages/Property/Property.jsx
@@ -8,6 +8,9 @@ import Collapse from "../../components/Collapse/Collapse";
 import list from "../../datas/listLocations";
 //css
 import "./property.scss"
+//images
+import starRate from "../../assets/star.svg"
+import star from "../../assets/star_grey.svg"
 
 
 const Property = () => {
@@ -15,6 +18,9 @@ const Property = () => {
     const {locId} = useParams();
 
     let rentProp = list.map((item) => item).find(item => item.id === locId);
+    const firstName = rentProp.host.name.split(" ")[0];
+    const lastName = rentProp.host.name.split(" ")[1];
+
     
     return (
         <div className="property">
@@ -22,28 +28,61 @@ const Property = () => {
            <Slideshow
                 list = {rentProp} 
             />
-
-            <h3 className="property-title">{rentProp.title}</h3>
-            <p className="">{rentProp.host.name}</p>
-            <img src ={rentProp.host.picture}/>
-            <p>{rentProp.location}</p>
-            <div className="property-tag">
-                {rentProp.tags.map((item)=> (
-                    <Tag 
-                        key ={item}
-                        tag = {item}/>
-                ))}
-            </div>
-            <div className="property-desc">
-            <Collapse 
-                title ="Description"
-                text = {rentProp.description}
-            />
-            <Collapse 
-                title = "Equipements"
-                text = {rentProp.equipments}
+        <section className="property-informations">
+        
+                <div className="property-left">
+                    <h3 className="property-title">{rentProp.title}</h3>
+                    <p className="property-location">{rentProp.location}</p>
                 
-            />
+                    <div className="property-tag">
+                        {rentProp.tags.map((item)=> (
+                            <Tag 
+                                key ={item}
+                                tag = {item}/>
+                        ))}
+                    </div>
+                </div>
+                
+                <div className="property-right">
+                    <div className="property-identity">
+                        <p className="property-host">
+                            <div>{firstName}</div>
+                            <div>{lastName}</div>
+                            </p>
+                        <img className="property-avatar" src ={rentProp.host.picture}/>
+            
+                    </div>
+            
+                    <div className="property-rating">
+                        <img src={star} alt="icon star rating" className="star-empty"/>
+                        <img src={star} alt="icon star rating" className="star-empty"/>
+                        <img src={star} alt="icon star rating" className="star-empty"/>
+                        <img src={star} alt="icon star rating" className="star-empty"/>
+                        <img src={star} alt="icon star rating" className="star-empty"/>
+                    </div>
+                </div>
+            
+           
+        </section>
+           
+            
+            <div className="property-desc">
+                <div className="property-desc-description">
+                    <Collapse 
+                    title ="Description"
+                    text = {rentProp.description}
+                />
+                </div>
+                <div className="property-desc-equipments">
+                    <Collapse 
+                    title = "Equipements"
+                    text = {rentProp.equipments.map((item)=>(
+                        <li className="equipment">{item}</li>
+                    ))}
+                
+                />
+                </div>           
+            
             </div>
         </div>
     )
