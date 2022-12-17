329ab7f4ae366f882628ac73da82fe537a5400b0
Chimda B <chimdabrignoli@gmail.com>
2022-12-17 01:06:50 +0100

    responsive homepage with components banner, card, gallery, footer and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,12 +13,11 @@ const Header = () => {
                 <Link to="/">
                     <img className="header-img-content" src={logo} alt="logo of Kasa" />
                 </Link>
-                
             </div>
 
             <nav className="header-nav">
                 <ul className="header-nav-list">
-                    <li >
+                    <li className="header-nav-list-li">
                         <NavLink 
                             className="header-nav-list-elt"
                             to="/"
@@ -35,7 +34,7 @@ const Header = () => {
                         to="/aboutUs"
                         style={({ isActive }) =>
                         isActive ? activeStyle : undefined
-                      }
+                        }
                         >
                             A Propos
                         </NavLink>
