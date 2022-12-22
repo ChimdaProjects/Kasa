cb8b76d85e299b3e66b1d82f553f94094c579ae9
Chimda B <chimdabrignoli@gmail.com>
2022-12-22 14:51:55 +0100

    modification collapsible's component

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -5,22 +5,11 @@ import chevronDown from "../../assets/chevron_down.svg";
 import chevronUp from "../../assets/chevron_up.svg";
 
 
-
 const Collapsible = ({title, text, namePage}) => {
 
     const [open, setOpen] = useState(false);
     const contentRef = useRef();
-    
-    //if (namePage === "about" ) {
-    //     height = contentRef.current.scrollHeight + "px";
-    //} else if(namePage== "property") {
-    //     height = 300 + "px";
-    //}
-   
-    //const pageAbout = namePage === "about" ? {height: contentRef.current.scrollHeight + "px"} : {height: 0+"px"};
-    //const pageProperty = namePage === "property" ? {height: 300+"px"} : {height: 0+"px"};
-
-    
+  
     return (
            
             <div className="collapse" >
@@ -33,35 +22,16 @@ const Collapsible = ({title, text, namePage}) => {
                         onClick={() => setOpen(!open)}
                     />  
                 </button>
-                {/* Condition for the height of the content if the page is about or property */}
-                {
-                    namePage === "about"  &&
-                    <div className="collapse-content-parent"
-                        ref={contentRef} 
-                        style = {open ? {height : contentRef.current.scrollHeight + "px"} : {height: "0px"}}  
-                    >
-                        <div className="collapse-content-text ">
-                            {text}
-                        </div>
+                <div className="collapse-content-parent"
+                ref={contentRef} 
+                style = {open ? {height : contentRef.current.scrollHeight + "px"} : {height: "0px"}} 
+                >
+                    <div className="collapse-content-text " style= {namePage === "property" ? {height:"315px"} : undefined}>
+                        {text}
                     </div>
-                }
-                {
-                    namePage === "property"  && 
-                    <div className="collapse-content-parent"
-                        ref={contentRef} 
-                        style = {open ? {height : "300px"} : {height: "0px"}}  
-                    >
-                        <div className="collapse-content-text " style={{height:"300px"}} >
-                            {text}
-                        </div>
-                    </div>
-                }
-               
-                
-                    
+                </div>
             </div> 
     )
-   
 }
 
 export default Collapsible;
